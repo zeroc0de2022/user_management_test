b3df089fe21fb4e8186bde5685b141cac3733862
zeroc0de <zeroc0de@mail.ru>
2024-12-10 18:16:30 +0300

    added error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -30,11 +35,9 @@ class Handler extends ExceptionHandler
      *
      * @var array<int, string>
      */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+    protected $dontFlash = ['current_password',
+                            'password',
+                            'password_confirmation',];
 
     /**
      * Register the exception handling callbacks for the application.
@@ -46,5 +49,31 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     * @param Request $request
+     * @param Throwable $exception
+     * @return \Illuminate\Http\Response|JsonResponse|Response
+     * @throws Throwable
+     */
+    public function render($request, Throwable $exception): \Illuminate\Http\Response|JsonResponse|Response
+    {
+        // Обработка ошибок валидации
+        if($exception instanceof ValidationException && $request->expectsJson()) {
+            return response()->json(['message' => 'Ошибка валидации',
+                                     'errors'  => $exception->errors()], 422);
+        }
+        // Обработка ошибки "Модель не найдена"
+        if($exception instanceof ModelNotFoundException && $request->expectsJson()) {
+            return response()->json(['message' => 'Ресурс не найден'], 404);
+        }
+        // Обработка других ошибок
+        if($request->expectsJson()) {
+            return response()->json(['message' => 'Что-то пошло не так',
+                                     'error'   => $exception->getMessage()], 500);
+        }
+        return parent::render($request, $exception);
     }
 }
